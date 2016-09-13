e8f13ffb6403a5f2e6ac01281a5d1f59dc1a8717
scamnasio <sara.camnasio@gmail.com>
2016-09-12 21:00:31 -0400

    Added some fun code to first p5 experiment

--- first_project/sketch.js
+++ first_project/sketch.js
@@ -1,7 +1,46 @@
 function setup() {
-  rect(0,0,100,200);
+  createCanvas(600,600);
+  frameRate(10);
+  println("Look I can code!");
 }
 
 function draw() {
+  //background(random(250), random(250), random(250));
+  background(250);
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(random(400), random(400), pmouseX, pmouseY);
+  stroke(random(255), random(255),random(255));
+  line(pmouseX,pmouseY,random(400), random(400));
+  
+  stroke(100);
+  ellipse(pmouseX,pmouseY, 50, 40);
   
 }
