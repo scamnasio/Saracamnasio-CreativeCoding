930784f200a42caddd1e0f58ae761f09860667f4
Sara Camnasio <sc6230@nyu.edu>
2016-12-05 18:28:45 -0500

    Add files via upload

--- data_viz/sketch.js
+++ data_viz/sketch.js
@@ -8,16 +8,47 @@ function preload() {
 
 function setup() {
   color(255);
-  createCanvas(400,600);
+  createCanvas(400,630);
   totalData = table.getColumn("Total");
   nailData = table.getColumn("Nail");
   skinData = table.getColumn("Skin");
+  
+  noStroke();
+  fill(255,110,205);
+  rect(0,0, 50,630);
+  fill(203,255,110);
+  rect(100,0, 50,630);
+  fill(255,250,110);
+  rect(150,0, 90,630);
+  fill(255,169,110);
+  rect(238,0, 90,630);
+  fill(255);
+  rect(0,0, 600,50);
+  fill(255);
+  rect(0,585, 600,50);
+  
   textSize(14);
+  fill("black");
   //stroke(255);
   //count the rows
+  
   text("Total", 10,560);
   text("Nail", 10,360);
   text("Skin", 10,160);
+  
+  text("Tue", 110,600);
+  text("Wed", 155,600);
+  text("Thu", 205,600);
+  text("Fri", 255,600);
+  text("Sat", 295,600);
+
+  textSize(30);
+  text("My hand biting problem", 10,30);
+  
+  
+  
+  stroke("black");
+  noFill();
   translate(40,530);
   for(var i =0; i < totalData.length; i++){
     translate(45,0);
@@ -32,7 +63,23 @@ function setup() {
     push();
     rect(20,-350,40, skinData[i] * -5.0);
     pop();
-    
-
+  
   }
 }
+
+//function draw(){
+  //if(mouseX > 45 || mouseX < 85){
+    //rec1 = true;
+  //} else {
+    //rec1 = false;
+  //}
+  
+  //while(rec1 === true)
+  //{
+    //fill("red");
+    //rect(45,0,40,600);
+  //}
+  //else {
+    //fill("blue")
+  //}
+//}
